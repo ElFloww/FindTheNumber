af7a6c529d2dc92ef1518c24e0d131249518afe6
Florent <florent.dubut@gmail.com>
2025-11-18 10:36:53 +0100

    Initial Commit

---
+++ FindTheNumber/FindTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheNumberApp.swift
+//  FindTheNumber
+//
+//  Created by Florent Dubut on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FindTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
